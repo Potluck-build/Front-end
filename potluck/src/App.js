948615f7f38004ecccfb6ca61ecd7cf3f770e59e
Javier Rosado <Javy@users-MacBook-Pro.local>
2021-12-20 12:53:32 -0500

    added routes and connected them

--- potluck/src/App.js
+++ potluck/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Header from "./components/Header";
+import AddFood from "./components/AddFood";
 
 const App = () => {
-  return <div className='App'>Potluck Planner</div>;
+  return (
+    <div className='App'>
+      Potluck Planner
+      <Header />
+      <Routes>
+        <Route path='/' component={Login} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/add-food' component={AddFood} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ potluck/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return <div className='links'></div>;
+};
+
+export default Header;
