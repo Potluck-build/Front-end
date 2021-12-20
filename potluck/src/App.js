391f8a5a1c38dad27b1319d6878938d25ebe9f0d
Javier Rosado <Javy@users-MacBook-Pro.local>
2021-12-20 17:18:55 -0500

    axiosWithAuth created

--- potluck/src/App.js
+++ potluck/src/App.js
@@ -7,84 +7,81 @@ import Header from "./components/Header";
 import AddFood from "./components/AddFood";
 import styled from "styled-components";
 
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
-import schema from './formValidatorSchema.js';
-import * as yup from 'yup';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import schema from "./formValidatorSchema.js";
+import * as yup from "yup";
 
 //////////////// INITIAL STATES ////////////////
 //////////////// INITIAL STATES ////////////////
 //////////////// INITIAL STATES ////////////////
 const initialFormValues = {
   ///// TEXT INPUTS /////
-  name: '',
-  email: '',
-  password: '',
-  confirmPassword: '',
-  
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+
   ///// CHECKBOX /////
   termsOfService: false,
-  
-}
+};
 const initialFormErrors = {
-  name: '',
-  email: '',
-  password: '',
-  confirmPassword: '',
-  termsOfService: '',
-}
-const initialUsers = []
-const initialDisabled = true
-
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+  termsOfService: "",
+};
+const initialUsers = [];
+const initialDisabled = true;
 
 const App = () => {
-  const [users, setUsers] = useState(initialUsers)          
-  const [formValues, setFormValues] = useState(initialFormValues) 
-  const [formErrors, setFormErrors] = useState(initialFormErrors) 
-  const [disabled, setDisabled] = useState(initialDisabled)       
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
 
   //////////////// HELPERS ////////////////
   //////////////// HELPERS ////////////////
   //////////////// HELPERS ////////////////
   const getUsers = () => {
+    axios
+      .get("https://reqres.in/api/users")
+      .then((resp) => {
+        setUsers(resp.data.data);
+      })
+      .catch((err) => console.error(err));
+  };
 
-    axios.get('https://reqres.in/api/users')
-      .then(resp => {
-        console.log(resp.data.data)
-          setUsers(resp.data.data)
-      }).catch(err => console.error(err))
-  }
-
-  const postNewUser = newUser => {
-   
-    axios.post('https://reqres.in/api/users', newUser)
-      .then(resp => {
-        console.log(resp)
-          setUsers([ resp.data, ...users ]);
-      }).catch( err => console.error(err))
-        .finally(() => setFormValues(initialFormValues))
-  }
-
+  const postNewUser = (newUser) => {
+    axios
+      .post("https://reqres.in/api/users", newUser)
+      .then((resp) => {
+        console.log(resp);
+        setUsers([resp.data, ...users]);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => setFormValues(initialFormValues));
+  };
 
   const validate = (name, value) => {
-    yup.reach(schema, name)
+    yup
+      .reach(schema, name)
       .validate(value)
-      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
-      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
-  }
+      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
+      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
+  };
 
   //////////////// EVENT HANDLERS ////////////////
   //////////////// EVENT HANDLERS ////////////////
   //////////////// EVENT HANDLERS ////////////////
   const inputChange = (name, value) => {
-  
     validate(name, value);
     setFormValues({
       ...formValues,
-      [name]: value 
-    })
-  }
+      [name]: value,
+    });
+  };
 
   const formSubmit = () => {
     const newUser = {
@@ -92,25 +89,25 @@ const App = () => {
       email: formValues.email.trim(),
       password: formValues.password.trim(),
       confirmPassword: formValues.confirmPassword.trim(),
-     
-      termsOfService: ['termsOfService'].filter(checkedterm => !!formValues[checkedterm]),
-    }
-   
+
+      termsOfService: ["termsOfService"].filter(
+        (checkedterm) => !!formValues[checkedterm]
+      ),
+    };
+
     postNewUser(newUser);
-  }
+  };
 
   //////////////// SIDE EFFECTS ////////////////
   //////////////// SIDE EFFECTS ////////////////
   //////////////// SIDE EFFECTS ////////////////
   useEffect(() => {
-    getUsers()
-  }, [])
+    getUsers();
+  }, []);
 
   useEffect(() => {
-    schema.isValid(formValues).then(valid => setDisabled(!valid))
-  }, [formValues])
-
-
+    schema.isValid(formValues).then((valid) => setDisabled(!valid));
+  }, [formValues]);
 
   return (
     <AppContainer>
@@ -119,12 +116,18 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register 
-                        values={formValues}
-                        change={inputChange}
-                        submit={formSubmit}
-                        disabled={disabled}
-                        errors={formErrors}/>} />
+          <Route
+            path='/register'
+            element={
+              <Register
+                values={formValues}
+                change={inputChange}
+                submit={formSubmit}
+                disabled={disabled}
+                errors={formErrors}
+              />
+            }
+          />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/add-food' element={<AddFood />} />
         </Routes>
